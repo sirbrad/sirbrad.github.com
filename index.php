8d3da4cb7294d88359a31a8920f42e5316310963
Bradley Few <few.bradley@gmail.com>
2012-03-01 20:52:13 +0000

    Cloned in my starting block

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!doctype html>
+<!--[if IE 7]><html class="no-js ie7" lang="en"><![endif]-->
 <!--[if IE 8]><html class="no-js ie8" lang="en"><![endif]-->
 <!--[if IE 9]><html class="no-js ie9" lang="en"><![endif]-->
 <!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>	
-    <title>Bradley Few . com</title>
+    <title><?= (basename($_SERVER['PHP_SELF']) === 'index.php') ? 'Welcome' : ucwords(str_replace(array('.php', '-'), array('', ' '), basename($_SERVER['PHP_SELF']))); ?> | Site Name</title>
     <script src="Assets/Scripts/Utils/modernizr.min.js"></script>
     <meta charset="utf-8">
     <meta name="author" content="sirBrad" />
@@ -15,6 +16,7 @@
     <![endif]-->
     <script src="Assets/Scripts/Utils/respond.min.js"></script>
 </head>
+<?php flush(); ?>
 <body>	
 	// Content
     <script data-main="Assets/Scripts/main" src="Assets/Scripts/Require.min.js"></script>
