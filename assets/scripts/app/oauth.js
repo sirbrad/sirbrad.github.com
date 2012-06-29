3c575863e62100e357409528a9bf9e5f27081797
Bradley Few <few.bradley@gmail.com>
2012-06-29 02:51:35 +0100

    Working on oauth posting

--- assets/scripts/app/oauth.js
+++ assets/scripts/app/oauth.js
@@ -1,16 +1,28 @@
 define(['jquery'], function($){
 
 
+	// Ok here is where I am.. I'm requesting permission when the user presses the btn.
+	// User gets taken to github, and return to me with a query param in url.
+	// I run a quick check for a param in url (run this everytime script runs atm) and store it as token
+	
+	// Now I need to post on the users behalf using the token and my secret key.. (not sure how to keep this secret either)
+	// Then I will do some basic js to set the username to the user_name and textarea to the body and so fourth.
+	
+	// some links to remind you: http://developer.github.com/v3/oauth/  |  https://github.com/LeaVerou/dabblet/blob/master/code/gist.js
+	
+	
+	// Then I can look into websockts to get it automating.
+	// Also would like to use xhr instead of jquery ajax.
+
+	var token;
+		
 	// Check to see if user is returning with
 	// a valid token
-	(function(){
-		var match = hash.match(/code=(\w+)/);
-          console.log(!!match && match[1]);
-	})
-
-	var clientID = '4e3a2ccec7c91a9eb26c';
+	(window.location.href.split('code=').length > 1) ? token = window.location.href.split('code=')[2] : listen();
 	
-	var authHost = 'https://github.com/login/oauth/authorize?client_id=' + clientID + '&redirect_uri=' + window.location.href + '&scope=public_repo';	
+
+	var clientID = '4e3a2ccec7c91a9eb26c',
+		authHost = 'https://github.com/login/oauth/authorize?client_id=' + clientID + '&redirect_uri=' + window.location.href + '&scope=public_repo';	
 
 	
 	$('.js-post').click(function(e){
