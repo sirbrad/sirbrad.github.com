2caf8499f4e06f1c0456f2ee7afba8317399510f
Bradley Few <few.bradley@gmail.com>
2012-06-29 02:16:45 +0100

    Working on oauth

---
+++ _site/assets/scripts/app/comment-listing.js
@@ -0,0 +1,58 @@
+define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
+	
+	var issueId = $('body').attr('data-issueID'),
+		commentListing = document.createElement('ol'),
+		container = $('.js-comments'),
+		comment;
+		
+	// Add ol attributes
+	commentListing.className = 'comment__listing';
+	
+	// Format date
+	function formatDate(str) {
+	
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			newDate = new Date(str),
+			currentDay = newDate.getDate(),
+			currentMonth = newDate.getMonth(),
+			currentYear = newDate.getFullYear();
+			
+		return currentDay + ' ' + months[currentMonth] + ' ' + currentYear;
+		
+	}
+	
+	
+	function setup(data){
+	
+		$.each(data, function(i){
+				
+			comment = tpl({
+				useravatar: data[i].user.avatar_url,
+				username: data[i].user.login,
+				url: data[i].user.url.split('users/')[1],
+				content: data[i].body_html,
+				date: formatDate(data[i].created_at),
+				commenturl: data[i].url
+			});
+			
+			commentListing.innerHTML += comment;
+		
+		
+		});
+		
+		// Append node
+		container.append(commentListing);
+		
+	};
+	
+	
+	// Make request
+	$.ajax('https://api.github.com/repos/sirbrad/sirbrad.github.com/issues/' + issueId + '/comments', {
+		headers: {Accept: "application/vnd.github.full+json"},
+		success: function(data) {
+			setup(data);
+		}
+	});
+	
+
+})
---
+++ _site/assets/scripts/app/oauth.js
@@ -0,0 +1,71 @@
+define(['jquery'], function($){
+
+	var clientID = '4e3a2ccec7c91a9eb26c';
+	
+	
+	
+	var authHost = 'https://github.com/login/oauth/authorize?client_id=' + clientID + '&scope=public_repo';
+	
+
+	
+	$('.js-post').click(function(e){
+	
+		var popup = open(authHost, 'popup');
+		
+		console.log(window.location)
+	
+	
+		e.preventDefault();
+	})
+	
+
+	
+	
+	
+	 /*  $(function () {
+        var extractToken = function(hash) {
+          var match = hash.match(/access_token=(\w+)/);
+          return !!match && match[1];
+        };
+          
+
+		
+	
+       var endUserAuthorizationEndpoint = authHost + "/connect";
+
+        var token = extractToken(document.location.hash);
+        if (token) {
+          $('div.authenticated').show();
+
+          $('span.token').text(token);
+
+          $.ajax({
+              url: resourceHost + '/me'
+            , beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', "OAuth " + token);
+                xhr.setRequestHeader('Accept',        "application/json");
+              }
+            , success: function (response) {
+                var container = $('span.user');
+                if (response) {
+                  container.text(response.username);
+                } else {
+                  container.text("An error occurred.");
+                }
+              }
+          });
+        } else {
+          $('div.authenticate').show();
+
+          var authUrl = endUserAuthorizationEndpoint + 
+            "?response_type=token" +
+            "&client_id="    + setting.clientId +
+            "&redirect_uri=" + window.location;
+
+          $("a.connect").attr("href", authUrl);
+        }
+      });*/
+	
+
+
+});
