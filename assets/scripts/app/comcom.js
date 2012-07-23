79eae961ba2cb9faac65425000541e00d5610118
Bradley Few <few.bradley@gmail.com>
2012-07-23 23:36:17 +0100

    Working version of github powered comments

--- assets/scripts/app/comcom.js
+++ assets/scripts/app/comcom.js
@@ -1,11 +1,14 @@
 define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
 
-
+	window.store = window.localStorage;
 
 	var container = $('.js-comments'),
-		bod = document.body,
-		path = 'https://api.github.com/repos/sirbrad/sirbrad.github.com/issues/' + bod.getAttribute('data-issueID') + '/comments',
-		listing;
+		clientID = '4e3a2ccec7c91a9eb26c',
+		issueID = document.body.getAttribute('data-issueID'),
+		path = 'https://api.github.com/repos/sirbrad/sirbrad.github.com/issues/' + issueID + '/comments',
+		listing,
+		textarea,
+		test;
 		
 	// Create the element that will hold the comments
 	listing = document.createElement('ol');
@@ -23,9 +26,38 @@ define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
 		
 	}
 	
+	function renderMarkdown(comment) {
+	
+		var input = {
+			'text': comment.body,
+			'mode': 'gfm'
+		}
+	
+		$.ajax({
+			url: 'https://api.github.com/markdown',
+			type: 'POST',
+			data: JSON.stringify(input),
+			success: function(data, status, jqXHR) {
+				
+				comment.body_html = data;
+				
+				var arr = []
+				
+				arr.push(comment);
+				
+				appendData(arr);
+				
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				//console.log(jqXHR, textStatus, errorThrown);
+			}
+		});
+
+	}
+	
 	
 	function appendData(data) {
-		
+
 		// Loop through the data and create elements using template
 		$.each(data, function(i){
 			
@@ -49,8 +81,6 @@ define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
 
 	}
 	
-	
-	
 	// Grab our comments
 	(function grabData() {
 	
@@ -67,6 +97,118 @@ define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
 		});
 		
 	}());
+	
+	
+
+
+	// Adding comments	
+	function postComment() {
+		
+		var comment = {
+				'body': textarea[0].value
+			};
+	
+		$.ajax({
+			url: 'https://api.github.com/repos/sirbrad/sirbrad.github.com/issues/' + issueID + '/comments',
+			type: 'POST',
+			data: JSON.stringify(comment),
+			dataType: 'json',
+			headers:  {
+				 Authorization: 'token '+ window.store.getItem('token'), 
+				 Accept: 'application/vnd.github.raw'
+				//'Content-Type': 'application/json; charset=UTF-8'
+			},
+			success: function(data, status, jqXHR){
+				
+				// We have to render to markdown before inserting
+				renderMarkdown(data);
+				
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				//console.log(jqXHR, textStatus, errorThrown);
+			}
+		});
+		
+	}
+	
+	function authenticateUser() {
+			
+		$.ajax({
+				url: 'https://api.github.com/user?access_token=' + window.store.getItem('token'),
+				type: 'GET',
+				success: function(data, status, jqXHR){
+
+					postComment();
+				}
+		})
+	
+	}
+	
+	function authenticateApp() {
+		
+		// Check if user has a token stored		
+		if (!window.store.getItem('token')) {
+		
+			var popup = open('https://github.com/login/oauth/authorize?client_id=' + clientID + '&scope=public_repo,user&redirect_uri=' + location.origin + '/login.html', 'popup', 'width=920,height=500');
+
+		} else {
+			authenticateUser();
+		}
+			
+	
+	}
+	
+	
+	// Validation
+	function validate(elem) {
+		
+		textarea = $(elem).find('textarea');
+		
+		if (textarea.val() === '') {
+			
+			elem.addClass('comment--error');
+			
+			textarea.on('keyup', function(){
+			
+				if (textarea.val() !== '') {
+					elem.removeClass('comment--error');
+				} 
+				
+			})
+			
+		} else {
+		
+			authenticateApp();
+		}
+	
+	}
+	
+	
+	// Comment Events
+	
+	$('.js-post-comment').on('click', 'input', function(e){
+	
+		// Validate this shit!
+		validate($(this).parent());
+		
+		e.preventDefault();
+	})
+	
+	
+	window.addEventListener('message', function (e) {
+		
+		$.ajax({
+			url: 'http://alanfewcompany.co.uk/oauth.php?code=' + e.data.split('&')[0],
+			type: 'GET',
+			success: function(token) {
+				
+				
+				window.store.setItem('token', token);
+			
+			}
+		})
+	})
+	
 
 
 
@@ -74,9 +216,7 @@ define(['jquery', 'tpl!../templates/comments.tpl'], function($, tpl){
 
 
 
-
-
-	/*window.store = window.localStorage;
+	/*
 
 	var bod = document.body,
 		listing,
